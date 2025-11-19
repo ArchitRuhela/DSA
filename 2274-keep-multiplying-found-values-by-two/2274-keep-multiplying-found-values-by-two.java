62827627bf79db5c8b86e8943e89269fce217120
ArchitRuhela <148525336+ArchitRuhela@users.noreply.github.com>
2025-11-19 22:46:01 +0530

    Time: 7 ms (7.96%), Space: 46.5 MB (7.36%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -19,18 +19,30 @@
 //     }
 // }
 
+// class Solution{
+//     public int findFinalValue(int[] nums, int original){
+//         HashMap<Integer, Integer> hush = new HashMap<Integer, Integer>();
+//         for(int i = 0 ; i < nums.length; i++){
+//             hush.put(nums[i],1);
+//         }
+//         while(true){
+//             if(hush.containsKey(original)){
+//                 original = original * 2;
+//             }
+//             else {
+//                 break;
+//             }
+//         }
+//         return original;
+//     }
+// }
+
 class Solution{
     public int findFinalValue(int[] nums, int original){
-        HashMap<Integer, Integer> hush = new HashMap<Integer, Integer>();
+        Arrays.sort(nums);
         for(int i = 0 ; i < nums.length; i++){
-            hush.put(nums[i],1);
-        }
-        while(true){
-            if(hush.containsKey(original)){
-                original = original * 2;
-            }
-            else {
-                break;
+            if(nums[i] == original){
+                original = 2 * original;
             }
         }
         return original;
